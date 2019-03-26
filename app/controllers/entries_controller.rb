32d331604969761a61b534f04bbeeb6bb99194af
James Dorr <jamesdorr3@gmail.com>
2019-03-26 14:55:34 -0400

    better add/delete buttons

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,17 +2,27 @@ class EntriesController < ApplicationController
    skip_before_action :verify_authenticity_token
 
   def create
-    @entry = Entry.create(entry_params)
+    @entry = Entry.create(create_entry_params)
     redirect_to @entry.tournament
+  end
+
+  def destroy
+    entry = Entry.find_by(destroy_entry_params)
+    entry.destroy
+    redirect_to entry.tournament
   end
 
   private
 
-  def entry_params
+  def create_entry_params
     params[:entry][:tournament_id] = params[:tournament_id]
     team = Team.find_or_create_by(name: params[:entry][:team])
     params[:entry][:team_id] = team.id
     params.require(:entry).permit(:tournament_id, :team_id)
+  end
+
+  def destroy_entry_params
+    params.permit(:team_id, :tournament_id)
   end
 
 end
