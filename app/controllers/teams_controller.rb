db69678bc39dee000d25691f14499a94d22efd26
James Dorr <jamesdorr3@gmail.com>
2019-03-26 17:34:13 -0400

    form magic

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,44 +1,59 @@
 class TeamsController < ApplicationController
-    
+
     before_action :find_team, only: [:show, :edit, :update, :destroy]
 
     def index
         @teams = Team.all
-    end 
+    end
 
     def show
     end
 
     def new
         @team = Team.new
-    end 
+        @team.players.build(name: 'player 1')
+    end
 
     def create
-        @team = Team.create(team_params)
-        redirect_to @team
-    end 
+      @team = Team.create(team_params)
+      @team.players << Player.find(player1_params)
+      @team.players << Player.find(player2_params)
+      redirect_to @team
+    end
 
     def edit
-    end 
+    end
 
     def update
         @team.update(team_params)
         redirect_to @team
-    end 
+    end
 
-    def destroy 
+    def destroy
         @team.destroy
         redirect_to teams_path
-    end 
+    end
 
     private
 
     def team_params
-        params.require(:team).permit(:name)
-    end 
+      params.require(:team).permit(:name)
+    end
 
     def find_team
         @team = Team.find(params[:id])
-    end 
+    end
+
+    def player1_params
+      params[:team][:players_attributes]["0"][:id]
+    end
+
+    def player2_params
+      params[:team][:players_attributes]["1"][:id]
+    end
+
+    # def player_params
+    #   p params["team"]["name"]["players_attributes"]["0"]["id"])
+    # end
 
 end
