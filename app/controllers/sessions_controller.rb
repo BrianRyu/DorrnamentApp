487599d1ae870ca1b86c1aa0e9a9ac7e06126c81
James Dorr <jamesdorr3@gmail.com>
2019-03-26 16:09:10 -0400

    basics of login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to controller: 'tournaments', action: 'index'
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
+
+end
