c0c25de701c9a8cc07deaf4bed9a103382ee975b
brianryu <s.brianryu@gmail.com>
2019-03-27 16:44:52 -0400

    line breaks in index and new views, fixed seed file for players

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,16 @@ Team.destroy_all
 Venue.destroy_all
 Tournament.destroy_all 
 
-mario = Player.create(name: 'Mario', gender: 'male', birth_date: "1981-09-13", start_date: "1991-09-13")
-luigi = Player.create(name: 'Luigi', gender: 'male', birth_date: "1985-9-13", start_date: "1995-9-13")
-princess_peach = Player.create(name: 'Princess Peach', gender: 'female', birth_date: "1985-11-13", start_date: "1999-12-13")
-toad = Player.create(name: 'Toad', gender: 'female', birth_date: "1991-3-13", start_date: "2000-1-13")
-yoshi = Player.create(name: 'Yoshi', gender: 'female', birth_date: "1983-8-13", start_date: "1997-4-27")
-bowser = Player.create(name: 'Bowser', gender: 'male', birth_date: "1999-9-13", start_date: "2011-11-13")
-princess_daisy = Player.create(name: 'Princess Daisy', gender: 'female', birth_date: "2003-9-13", start_date: "2010-9-13")
-rosalina = Player.create(name: 'Rosalina', gender: 'female', birth_date: "0000-01-01", start_date: "2000-9-13")
-donkey_kong = Player.create(name: 'Donkey Kong', gender: 'male', birth_date: "1982-10-13", start_date: "2005-9-13")
-diddy_kong = Player.create(name: 'Diddy Kong', gender: 'male', birth_date: "1983-9-13", start_date: "2003-9-13")
+mario = Player.create!(name: 'Mario', gender: 'male', birth_date: "1981-09-13", start_date: "1991-09-13", password: "pw")
+luigi = Player.create!(name: 'Luigi', gender: 'male', birth_date: "1985-9-13", start_date: "1995-9-13", password: "pw")
+princess_peach = Player.create!(name: 'Princess Peach', gender: 'female', birth_date: "1985-11-13", start_date: "1999-12-13", password: "pw")
+toad = Player.create!(name: 'Toad', gender: 'female', birth_date: "1991-3-13", start_date: "2000-1-13", password: "pw")
+yoshi = Player.create!(name: 'Yoshi', gender: 'female', birth_date: "1983-8-13", start_date: "1997-4-27", password: "pw")
+bowser = Player.create!(name: 'Bowser', gender: 'male', birth_date: "1999-9-13", start_date: "2011-11-13", password: "pw")
+princess_daisy = Player.create!(name: 'Princess Daisy', gender: 'female', birth_date: "2003-9-13", start_date: "2010-9-13", password: "pw")
+rosalina = Player.create!(name: 'Rosalina', gender: 'female', birth_date: "0000-01-01", start_date: "2000-9-13", password: "pw")
+donkey_kong = Player.create!(name: 'Donkey Kong', gender: 'male', birth_date: "1982-10-13", start_date: "2005-9-13", password: "pw")
+diddy_kong = Player.create!(name: 'Diddy Kong', gender: 'male', birth_date: "1983-9-13", start_date: "2003-9-13", password: "pw")
 
 brobro = Team.create(name: "BroBro")
 princesses = Team.create(name: "Princesses")
