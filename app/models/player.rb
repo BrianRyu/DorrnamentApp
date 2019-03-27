03570e1e550cc71c734c3cd1dc362175a8dad402
brianryu <s.brianryu@gmail.com>
2019-03-27 10:21:39 -0400

    Index Page for Drafts

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,6 @@ class Player < ApplicationRecord
     has_many :teams, through: :drafts
     has_many :entries, through: :teams
     has_many :tournaments, through: :entries
+
+    has_secure_password
 end
