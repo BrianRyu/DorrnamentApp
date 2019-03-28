754a9f78601d1dc776daa670567990abb173a9c9
James Dorr <jamesdorr3@gmail.com>
2019-03-28 16:37:40 -0400

    beautiful styling

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,7 +5,7 @@ class Player < ApplicationRecord
     has_many :entries, through: :teams
     has_many :tournaments, through: :entries
 
-    validates :name, :birth_date, :email, :password, presence: true
+    validates :name, :birth_date, :email, presence: true
     validates :img_url, format: {with: /(\A(https*\/\/).+(\..+)\z)?/}
 
 end
