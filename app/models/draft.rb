f5d7df6fef3b775205c80d0850a2170270e706f2
James Dorr <jamesdorr3@gmail.com>
2019-03-29 14:34:41 -0400

    fixed edit bug

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -3,5 +3,16 @@ class Draft < ApplicationRecord
   belongs_to :team
 
   validates :player_id, :team_id, presence: true
-\
+  # validate :draft_must_be_unique, on: :create
+
+  def tuid
+    self.player_id.to_s + self.team_id.to_s
+  end
+
+  # def draft_must_be_unique
+  #   if Draft.all.map(&:tuid).include?(self.tuid)
+  #     errors.add(:discount, "can't be greater than total value")
+  #   end
+  # end
+
 end
